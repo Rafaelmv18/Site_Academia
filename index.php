be60e1ec1cd1171ca2e6a6c4a690cadecd1d9371
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2025-09-09 09:24:41 -0300

    error page

--- index.php
+++ index.php
@@ -35,6 +35,8 @@
         <?php
             if(file_exists('pages/'.$url.'.php')){
                 include('pages/'.$url.'.php');
+            }else{
+                include('pages/404.php');
             }
         ?>
         
