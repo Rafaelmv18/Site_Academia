58febacc1c51379bb73a62286a4774c9e35241b1
lucasbfaria <lucasbuenodefaria@gmail.com>
2025-09-14 19:30:16 -0300

    Alert

--- classes/Painel.php
+++ classes/Painel.php
@@ -21,6 +21,33 @@ class Painel{
 		$busca->execute();
 		return $busca = $busca->fetchAll();
 	}
+	public static function alert($tipo,$mensagem){
+		if($tipo == 'sucesso'){
+			echo '
+				<div class="alert alert-success" role="alert" style="z-index: 99; top: 1vh; position: fixed; left: 75vw;">
+					<i class="bi bi-check-circle me-1" style="margin-right: 5px;"></i>'
+					.$mensagem.
+					'
+					<span class="progress"></span>
+				</div>';
+		}else if($tipo == 'atencao'){
+			echo '
+				<div class="noprint alert alert-warning" role="alert" style="z-index: 99; top: 1vh; position: fixed; left: 75vw;">
+					<i class="bi bi-exclamation-triangle me-1" style="margin-right: 5px;"></i>'
+					.$mensagem.
+					'
+					<span class="progress"></span>
+				</div>';
+		}else if($tipo == 'erro'){
+			echo '
+				<div class="alert alert-danger" role="alert" style="z-index: 99; top: 1vh; position: fixed; left: 75vw;">
+					<i class="bi bi-exclamation-octagon me-1" style="margin-right: 5px;"></i>'
+					.$mensagem.
+					'
+					<span class="progress"></span>
+				</div>';
+		}
+	}
 }
 
 ?>
