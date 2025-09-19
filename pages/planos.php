64cc9671402c0e48c9d77c4ebf8265ed70d588bf
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2025-09-19 16:58:49 -0300

    planos

--- pages/planos.php
+++ pages/planos.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'config.php'; 
+$planos = Painel::selectAll('plano', 'plano_id', 'ASC');
+?>
 <section class="sobre">
     <h2>Transforme Seu Corpo e Mente</h2>
     <p>
@@ -11,40 +15,17 @@
 <section class="planos">
     <h2>Escolha o Plano Perfeito para Você</h2>
     <div class="planos-container">
-        <div class="plano">
-            <h3>Plano Básico</h3>
-            <p class="preco">R$ 79,90<span>/mês</span></p>
-            <ul class="beneficios">
-                <li><i class="fa-solid fa-check"></i> Acesso completo à área de musculação</li>
-                <li><i class="fa-solid fa-check"></i> Acesso em horário comercial</li>
-                <li><i class="fa-solid fa-check"></i> Avaliação física inicial</li>
-            </ul>
-            <button class="btn-primary" onclick="window.location.href='login.php'">Quero Começar</button>
-        </div>
-
-        <div class="plano destaque">
-            <div class="selo-destaque">Mais Popular</div>
-            <h3>Plano Plus</h3>
-            <p class="preco">R$ 109,90<span>/mês</span></p>
-            <ul class="beneficios">
-                <li><i class="fa-solid fa-check"></i> Acesso completo à área de musculação</li>
-                <li><i class="fa-solid fa-check"></i> Todas as aulas coletivas (Fit Dance, Pilates, etc.)</li>
-                <li><i class="fa-solid fa-check"></i> Horário livre (06h às 22h)</li>
-                <li><i class="fa-solid fa-check"></i> Acesso a aplicativo de treinos</li>
-            </ul>
-            <button class="btn-primary" onclick="window.location.href='login.php'">Escolher Plano</button>
-        </div>
-
-        <div class="plano">
-            <h3>Plano Premium</h3>
-            <p class="preco">R$ 159,90<span>/mês</span></p>
-            <ul class="beneficios">
-                <li><i class="fa-solid fa-check"></i> Todos os benefícios do Plano Plus</li>
-                <li><i class="fa-solid fa-check"></i> Acompanhamento com Personal Trainer 2x/semana</li>
-                <li><i class="fa-solid fa-check"></i> Avaliação física e nutricional mensal</li>
-                <li><i class="fa-solid fa-check"></i> Acesso para um amigo por mês</li>
-            </ul>
-            <button class="btn-primary" onclick="window.location.href='login.php'">Seja Premium</button>
-        </div>
+        <?php foreach ($planos as $plano){?>
+            <div class="plano">
+                <h3><?php echo $plano['nome']; ?></h3>
+                <p class="preco"><?php echo $plano['valor']; ?><span>/mês</span></p>
+                <ul class="beneficios">
+                    <li><i class="fa-solid fa-check"></i> Acesso completo à área de musculação</li>
+                    <li><i class="fa-solid fa-check"></i> Acesso em horário comercial</li>
+                    <li><i class="fa-solid fa-check"></i> Avaliação física inicial</li>
+                </ul>
+                    <button class="btn-primary" onclick="window.location.href='login.php'">Quero Começar</button>
+            </div>
+        <?php } ?>
     </div>
 </section>
--- pages_painel/planos.php
+++ pages_painel/planos.php
@@ -5,49 +5,6 @@ $planos = Painel::selectAll('plano', 'plano_id', 'ASC');
 <section class="planos">
     <h2>Escolha o Plano Perfeito para Você</h2>
     <div class="planos-container">
-        <div class="plano">
-            <h3>Plano Básico</h3>
-            <p class="preco">R$ 79,90<span>/mês</span></p>
-            <ul class="beneficios">
-                <li><i class="fa-solid fa-check"></i> Acesso completo à área de musculação</li>
-                <li><i class="fa-solid fa-check"></i> Acesso em horário comercial</li>
-                <li><i class="fa-solid fa-check"></i> Avaliação física inicial</li>
-            </ul>
-            <button class="btn-primary" onclick="window.location.href='pages_painel/pagamento.php?plano=Basico&valor=79.90'">Quero Começar</button>
-
-
-        </div>
-
-        <div class="plano destaque">
-            <div class="selo-destaque">Mais Popular</div>
-            <h3>Plano Plus</h3>
-            <p class="preco">R$ 109,90<span>/mês</span></p>
-            <ul class="beneficios">
-                <li><i class="fa-solid fa-check"></i> Acesso completo à área de musculação</li>
-                <li><i class="fa-solid fa-check"></i> Todas as aulas coletivas (Fit Dance, Pilates, etc.)</li>
-                <li><i class="fa-solid fa-check"></i> Horário livre (06h às 22h)</li>
-                <li><i class="fa-solid fa-check"></i> Acesso a aplicativo de treinos</li>
-            </ul>
-            <button class="btn-primary" onclick="window.location.href='pages_painel/pagamento.php?plano=Basico&valor=109.90'">Escolher Plano</button>
-
-        </div>
-
-        <div class="plano">
-            <h3>Plano Premium</h3>
-            <p class="preco">R$ 159,90<span>/mês</span></p>
-            <ul class="beneficios">
-                <li><i class="fa-solid fa-check"></i> Todos os benefícios do Plano Plus</li>
-                <li><i class="fa-solid fa-check"></i> Acompanhamento com Personal Trainer 2x/semana</li>
-                <li><i class="fa-solid fa-check"></i> Avaliação física e nutricional mensal</li>
-                <li><i class="fa-solid fa-check"></i> Acesso para um amigo por mês</li>
-            </ul>
-            <button class="btn-primary" onclick="window.location.href='pages_painel/pagamento.php?plano=Premium&valor=159.90'">Seja Premium</button>
-        </div>
-    </div>
-
-
-    <h2>Escolha o Plano Perfeito para Você</h2>
-    <div class="planos-container">
         <?php foreach ($planos as $plano){?>
         <div class="plano">
             <h3><?php echo $plano['nome']; ?></h3>
