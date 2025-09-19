54c7a1eb16690af9ecfdb8eac1382dffceafa3e8
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2025-09-19 16:58:41 -0300

    modalidades

--- pages/modalidades.php
+++ pages/modalidades.php
@@ -1,72 +1,36 @@
+<?php
+require_once 'config.php'; 
+$modalidades = Painel::selectAll('modalidade', 'modalidade_id', 'ASC');
+?>
 <section class="modalidades">
     <div class="container">
         <div class="intro">
             <h1>Transforme Seu Corpo</h1>
             <p>Descubra nossas atividades e comece sua jornada fitness hoje mesmo</p>
         </div>
-        <div class="card">
-            <div class="card-inner">
-                <div class="card-front">
-                    <img src="img/musculacao.jpg" alt="Musculação" class="card-image">
-                    <div class="card-title">Musculação 24h</div>
-                </div>
-                <div class="card-back">
-                    <div class="card-description">
-                        <h3>Musculação 24h</h3>
-                        <p>Treine a qualquer hora com equipamentos de última geração e conquiste seus objetivos.</p>
-                        <ul>
-                            <li>Acesso 24 horas por dia</li>
-                            <li>Equipamentos modernos</li>
-                            <li>Ambiente climatizado</li>
-                            <li>Instrutores qualificados</li>
-                            <li>Programas personalizados</li>
-                        </ul>
+        <?php foreach ($modalidades as $modalidade){ ?>
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <img src="<?= htmlspecialchars($modalidade['imagem']) ?>" 
+                             alt="<?= htmlspecialchars($modalidade['nome']) ?>" class="card-image">
+                        <div class="card-title"><?php echo $modalidade['nome']?></div>
+                    </div>
+                    <div class="card-back">
+                        <div class="card-description">
+                            <h3><?php echo $modalidade['nome']?></h3>
+                            <p><?php echo $modalidade['descricao']?></p>
+                            <ul>
+                                <li>Acesso 24 horas por dia</li>
+                                <li>Equipamentos modernos</li>
+                                <li>Ambiente climatizado</li>
+                                <li>Instrutores qualificados</li>
+                                <li>Programas personalizados</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div class="card">
-            <div class="card-inner">
-                <div class="card-front">
-                    <img src="img/artesmarciais.jpg" alt="Artes Marciais" class="card-image">
-                    <div class="card-title">Artes Marciais</div>
-                </div>
-                <div class="card-back">
-                    <div class="card-description">
-                        <h3>Artes Marciais</h3>
-                        <p>Desenvolva disciplina, força e técnica com nossas modalidades de artes marciais.</p>
-                        <ul>
-                            <li>Karatê e Taekwondo</li>
-                            <li>Jiu-Jitsu Brasileiro</li>
-                            <li>Muay Thai</li>
-                            <li>Instrutores experientes</li>
-                            <li>Turmas para todas as idades</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="card-inner">
-                <div class="card-front">
-                    <img src="img/pilates.jpg" alt="Pilates" class="card-image">
-                    <div class="card-title">Pilates</div>
-                </div>
-                <div class="card-back">
-                    <div class="card-description">
-                        <h3>Pilates</h3>
-                        <p>Fortaleça seu corpo, melhore a postura e aumente a flexibilidade com movimentos que integram mente e corpo.</p>
-                        <ul>
-                            <li>Fortalecimento do core (centro de força)</li>
-                            <li>Melhora da postura e alinhamento corporal</li>
-                            <li>Aumento da flexibilidade e tônus muscular</li>
-                            <li>Redução de estresse e dores crônicas</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php } ?>
     </div>
 </section>
